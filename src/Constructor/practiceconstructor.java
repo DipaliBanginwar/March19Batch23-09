cbb8dfffd31b43b527bd7e8ec007930b4f967f1f
RahulGandhale <gandhalerahul4@gmail.com>
2022-09-23 14:59:50 +0530

    This is change made by Rahul

--- src/Constructor/practiceconstructor.java
+++ src/Constructor/practiceconstructor.java
@@ -1,6 +1,7 @@
 package Constructor;
 
-public class practiceconstructor {
+public class practiceconstructor //This is constructor program
+{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -12,7 +13,7 @@ public class practiceconstructor {
 		System.out.println("without paratmeter");
 	}
 	
-	public newtest1(int a,int b,int c)
+	//public newtest1(int d, int b, int c)
 	{
 		System.out.println("with parameter");
 	}
