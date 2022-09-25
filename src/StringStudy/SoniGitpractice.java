61d6e2eb61d2aa3932bfd0c3b20f1c472cdebe9a
SoniRagho <sonirragho@gmail.com>
2022-09-25 23:19:17 +0530

    This changes made by soni ragho 25-09-2022

---
+++ src/StringStudy/SoniGitpractice.java
@@ -0,0 +1,11 @@
+package StringStudy;
+
+public class SoniGitpractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("soni git practice");
+
+	}
+
+}
